84cac38f0fefb8330e348d2fdd4bac41075dd600
tjashaG <68466550+tjashaG@users.noreply.github.com>
2020-11-02 11:14:45 +0100

    Update mood_checker.py

--- mood_checker.py
+++ mood_checker.py
@@ -1,12 +1,18 @@
 name = input("What's your name? ")
 print(f"Hi, {name}!")
-mood = input("How you doing' today? (fantastic, happy, meh or sad)")
+mood = input("How you doing' today? ")
+mood_lower = mood.lower()
+positive = ["well", "good", "super", "great", "wonderful", "superb", "excellent", "fantastic"]
+negative = ["not good", "poor", "sad", "hurt", "bad", "unwell", "ill", "rotten", "awful", "lame", "horrible"]
+neutral = ["so-so", "ok", "meh"]
 
-if mood == "happy":
-    print(f"Keep up the good work, {name}")
-elif mood == "fantastic":
-    print(f"Great to hear {name}, you're doing something right!")
-elif mood == "meh":
-    print(f"That's ok, we all have days like these. It'll get better, {name}!")
-else:
-    print(f"Aww, {name}, I'm so sorry! Virtual hug is loading...\nnow!")
+for i in positive:
+    if i == mood_lower:
+        print(f"Keep up the good work, {name}")
+for i in neutral:
+    if i == mood_lower:
+        print(f"That's ok, we all have days like these. It'll get better, {name}!")
+for i in negative:
+    if i == mood_lower:
+        print(f"Aww, {name}, I'm so sorry! Virtual hug is loading...\nnow!")
+
